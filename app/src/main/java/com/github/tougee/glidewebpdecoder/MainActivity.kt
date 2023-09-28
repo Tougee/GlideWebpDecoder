5833554e99d8995ebeb510a7fc12ef99169b63d1
touge <touge1027@gmail.com>
2023-09-28 23:58:54 +0800

    initial commit

---
+++ app/src/main/java/com/github/tougee/glidewebpdecoder/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.github.tougee.glidewebpdecoder
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.github.tougee.glidewebpdecoder.ui.theme.GlideWebpDecoderTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GlideWebpDecoderTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GlideWebpDecoderTheme {
+        Greeting("Android")
+    }
+}
